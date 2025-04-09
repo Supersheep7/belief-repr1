caadb6c552d88429513279c4511f07120e7041e4
Supersheep7 <alessandro.corona.m@gmail.com>
2025-04-09 16:26:21 +0200

    Worked on logit attribution

--- utils.py
+++ utils.py
@@ -2,9 +2,10 @@
 import torch
 import torch.nn as nn
 import einops
+from fancy_einsum import einsum
 import tqdm.auto as tqdm
 import plotly.express as px
-from plotly.utils import imshow, line
+from plotly.express import imshow, line
 import transformer_lens.utils as utils
 from transformer_lens.hook_points import (
     HookPoint,
@@ -31,7 +32,7 @@ class LogitAttribution:
     ) -> float:
         
         scaled_residual_stack = cache.apply_ln_to_stack(residual_stack, layer=-1, pos_slice=-1)
-        return einops.einsum(
+        return einsum(
             "... batch d_model, batch d_model -> ...",
             scaled_residual_stack,
             logit_diff_directions,
@@ -39,17 +40,19 @@ class LogitAttribution:
 
     def plot_logit_diffs(self, data, x=None, hover_name=None, title=None):
         
+        if isinstance(data, torch.Tensor):
+            data = data.detach().cpu().numpy() 
+        
         line(
             data,
             x=x,
             hover_name=hover_name,
             title=title,
-        )
+        ).show()
 
     def compute(self, 
-                cache: ActivationCache, 
                 prompts: list, 
-                answers: list = ["True", "False"]
+                answers: list
     ) -> None:
         
         # Tokenize answers, tokenize prompts, get cache on a fwd pass
@@ -62,11 +65,11 @@ class LogitAttribution:
         logit_diff_directions = (answer_residual_directions[:, 0] - answer_residual_directions[:, 1])
         
         # Get cumulative difference
-        accumulated_residual, labels = cache.accumulated_resid(layer=-1, incl_mid=True, pos_slice=-1, return_labels=True)
+        accumulated_residual, self.logit_lens_labels = cache.accumulated_resid(layer=-1, incl_mid=True, pos_slice=-1, return_labels=True)
         self.logit_lens_logit_diffs = self.residual_stack_to_logit_diff(accumulated_residual, cache, logit_diff_directions, prompts)
 
         # Get per layer difference
-        per_layer_residual, labels = cache.decompose_resid(layer=-1, pos_slice=-1, return_labels=True)
+        per_layer_residual, self.per_layer_labels = cache.decompose_resid(layer=-1, pos_slice=-1, return_labels=True)
         self.per_layer_logit_diffs = self.residual_stack_to_logit_diff(per_layer_residual, cache, logit_diff_directions, prompts)
 
         return 
@@ -87,3 +90,15 @@ class LogitAttribution:
         )
 
         return
+    
+    def get_attribution(self):
+
+        print("Logit lens diffs and labels:", 
+              self.logit_lens_logit_diffs,
+              self.logit_lens_labels)
+
+        print("Per layer diffs and labels:", 
+              self.per_layer_logit_diffs,
+              self.per_layer_labels)
+
+        return self.logit_lens_logit_diffs, self.logit_lens_labels, self.per_layer_logit_diffs, self.logit_lens_logit_diffs
